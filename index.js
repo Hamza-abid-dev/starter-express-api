c2fc78caf5361cef53fd96cf620d7c099ccf9423
Hamza Abid <149709322+Hamza-abid-dev@users.noreply.github.com>
2024-03-13 11:05:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+  res.json(Api)
 })
+app.get('/:id', (req, res) => {
+  const id = +req.params.id;
+  const card = Api.filter(card => card.id === id);
+  res.json(card);
+}) 
 app.listen(process.env.PORT || 3000)
